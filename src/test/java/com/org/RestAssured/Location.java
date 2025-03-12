efadd9f28cf4ef2e00811a76046c5e0db231f346
gopi2346 <sgopi2346@gmail.com>
2025-03-12 11:14:41 +0530

    New Changes

---
+++ src/test/java/com/org/RestAssured/Location.java
@@ -0,0 +1,13 @@
+package com.org.RestAssured;
+
+public class Location {
+		
+		private int lat;
+		private int lng;
+		
+	public Location (int lat,int lng) {
+		this.lat = lat;
+		this.lng = lng;
+	}
+		
+}
